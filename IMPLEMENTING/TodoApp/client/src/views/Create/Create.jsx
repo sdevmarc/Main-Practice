6d14d65a85e6ceee20a29b042a8ba61cfe3cd51e
sdevmarc <suanieeee@yahoo.com>
2023-11-12 22:38:58 +0800

    Made create page template

---
+++ IMPLEMENTING/TodoApp/client/src/views/Create/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Create = () => {
+  return (
+    <div className="Create">
+        This is Create
+    </div>
+  )
+}
+
+export default Create
