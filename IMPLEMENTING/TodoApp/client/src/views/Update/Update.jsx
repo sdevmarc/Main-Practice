352df282a78ec28dc1e0c1747e0b608492e71e99
sdevmarc <suanieeee@yahoo.com>
2023-11-12 22:39:10 +0800

    Made update page template

---
+++ IMPLEMENTING/TodoApp/client/src/views/Update/Update.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Update = () => {
+  return (
+    <div className="Update">
+        This is update
+    </div>
+  )
+}
+
+export default Update
