fe23fbb8dffc1ac4b7e0974bc832d8f5e7449bb4
sdevmarc <suanieeee@yahoo.com>
2023-11-12 22:38:32 +0800

    Made routes for each page

--- IMPLEMENTING/TodoApp/client/src/main.jsx
+++ IMPLEMENTING/TodoApp/client/src/main.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './views/Home/Home'
+import Create from './views/Create/Create'
+import Read from './views/Read/Read'
+import Update from './views/Update/Update'
+import Delete from './views/Delete/Delete'
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/create',
+    element: <Create />
+  },
+  {
+    path: '/read',
+    element: <Read />
+  },
+  {
+    path: '/update',
+    element: <Update />
+  },
+  {
+    path: '/delete',
+    element: <Delete />
   }
 ])
 
