5939984c444b7a2a5d2c0286679d6ab9f2f650a6
sdevmarc <suanieeee@yahoo.com>
2023-11-12 22:38:51 +0800

    Made delete page template

---
+++ IMPLEMENTING/TodoApp/client/src/views/Delete/Delete.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+const Delete = () => {
+  return (
+    <div className="Delete">
+        This is Delete
+    </div>
+  )
+}
+
+export default Delete
