d815aea84ce9f5405cd81812220de0307ccd0e96
sdevmarc <suanieeee@yahoo.com>
2024-01-01 14:56:10 +0800

    first commit tailwind

---
+++ Christmas Practice/Websites/React/Tailwind/Airbnb/client/src/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home'
+
+const Routes = createBrowserRouter([
+    {
+        path: '/', element: <Home />
+    }
+])
+
+export default Routes
